a28a937c06b11f5391b4962d59f3f35cac8c75e5
Kojiro Y <kojiroy@usc.edu>
2024-03-23 23:56:20 -0700

    Created linear velocity controller [untested]

--- controllers/src/linear_velocity_controller.cpp
+++ controllers/src/linear_velocity_controller.cpp
@@ -4,55 +4,92 @@ Controls AUV movement in x, y, z direction
 */
 
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include <ros/console.h>
+#include "thruster_msgs/ThrustOutput.h"
 #include "std_msgs/Float64.h"
+#include <cmath>
+
+ros::Publisher linear_vel_pub;
 
 uint16_t linear_vel_size = 1; //size of linear velocity messages queue
 uint16_t thrusters_size = 1; //size of thruster messages queue
+uint8_t* thruster_mapping;
+double* thruster_values;
 
-void linear_vel_x_callback(const std_msgs::String::ConstPtr& msg) {
-    ROS_INFO("I heard: [%s]", msg->data.c_str());
+bool update = false;
+int x_vel = 0;
+int y_vel = 0;
+int z_vel = 0;
+
+void linear_vel_x_callback(const std_msgs::Float64::ConstPtr& data) {
+    update = true;
+    x_vel = data->data;
 }
 
-void linear_vel_y_callback(const std_msgs::String::ConstPtr& msg) {
-    ROS_INFO("I heard: [%s]", msg->data.c_str());
+void linear_vel_y_callback(const std_msgs::Float64::ConstPtr& data) {
+    update = true;
+    y_vel = data->data;
 }
 
-void linear_vel_z_callback(const std_msgs::String::ConstPtr& msg) {
-    ROS_INFO("I heard: [%s]", msg->data.c_str());
+void linear_vel_z_callback(const std_msgs::Float64::ConstPtr& data) {
+    update = true;
+    z_vel = data->data;
+}
+
+void thruster_mixer() {
+    int max = fmax(1, fabs(x_val) + fabs(y_vel));
+    // Normalize
+    // Max is always between 1 and 2. z_vel doesn't contribute to it
+    thruster_values[0] = (x_val - y_vel)/max;
+    thruster_values[1] = (x_val + y_vel)/max;
+    thruster_values[2] = (-x_vel + y_vel)/max;
+    thruster_values[3] = (-x_vel - y_vel)/max;
+    thruster_values[4] = z_vel;
+    thruster_values[5] = z_vel;
+    thruster_values[6] = z_vel;
+    thruster_values[7] = z_vel;
 }
 
 int main (int argc, char **argv) {
     ros::init(argc, argv, "linear_velocity_controller");
+    if( ros::console::set_logger_level(ROSCONSOLE_DEFAULT_NAME, ros::console::levels::Debug) ) {
+        ros::console::notifyLoggerLevelsChanged();
+    }
 
     ros::NodeHandle linear_vel_node;
 
     ros::Subscriber linear_vel_x = linear_vel_node.subscribe("/linear_vel/x", linear_vel_size, lin_vel_x_callback);
     ros::Subscriber linear_vel_y = linear_vel_node.subscribe("/linear_vel/y", linear_vel_size, lin_vel_y_callback);
     ros::Subscriber linear_vel_z = linear_vel_node.subscribe("/linear_vel/z", linear_vel_size, lin_vel_z_callback);
-
-    ros::spin(); // ???
-
-    //TODO do stuff with the data acquired
-    //allocate the thrusters... the thrust
-    // all normalized to -1 to 1
-    // thrusters at 30 degree angles
     
-    ros::Publisher linear_vel_pub = linear_vel_node.advertise<std_msgs::Float64>("/thrusters/lv_control_input", thrusters_size);
+    linear_vel_pub = linear_vel_node.advertise<thruster_msgs::ThrustOutput>("/thrusters/lv_control_input", thrusters_size);
 
     ros::Rate loop_rate(10); //todo fix magic number
 
+    // Initialize thruster mapping values and thruster values
+    thruster_mapping = new uint8_t[8];
+    thruster_values = new double[8];
+    for (int i = 0; i<8; i++) {
+        ros::param::param<std::int>("thruster_"+str(i), thruster_mapping[i], i);
+        thruster_values[i] = 0;
+    }
+
+    thruster_msgs::ThrustOutput msg;
+
     while(ros::ok()) {
-        std_msgs::Float64 linear_vel_msg;
+        //TODO:SOMONE if no new inputs sent after 5s, turn off motors
 
-        // acquire data from linear vel numbers
-
-        linear_vel_pub.publish(linear_vel_msg);
-
-        //if no new inputs sent after 5s, turn off motors
-
+        if (update) {
+            thruster_mixer();
+            for (int i = 0; i<8; i++) {
+                msg.num = thruster_mapping[i];
+                msg.val = thruster_values[i];
+                linear_vel_pub.publish(msg);
+            }
+            ROS_DEBUG("Published to %i thrust val %f", msg.num, msg.val);
+            update = false;
+        }
         ros::spinOnce();
-        loop_rate.sleep();
     }
 
     return 0;
