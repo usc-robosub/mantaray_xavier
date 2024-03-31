e7e0cb439c6dac89d70eba0106a385351f9f66d8
Kojiro Y <kojiroy@usc.edu>
2024-03-31 22:32:09 +0100

    Create tranforms for real life

--- docker_scripts/init.sh
+++ docker_scripts/init.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-
+chmod o+rw /dev/i2c-0
 source ~/.bashrc
 source ~/catkin_ws/devel/setup.bash
-export ROS_IP=192.168.23.101
+export ROS_IP=192.168.23.105
 export ROS_MASTER_URI=http://lab_computer:11311
 #export ROS_MASTER_URI=http://localhost:11311
-export ROS_HOSTNAME=mantaraylab1
+export ROS_HOSTNAME=mantaray
+#export ROS_HOSTNAME=localhost
 export PYTHONPATH=/home/mantaray/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH
