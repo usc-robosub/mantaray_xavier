d211bf7f2ec2e0d72733be336e8f6e93a1861624
Kojiro Y <kojiroy@usc.edu>
2024-04-09 03:45:47 +0100

    Created a launch file for the controllers and a init bash script for catkin stuff

--- docker_scripts/init.sh
+++ docker_scripts/init.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
-chmod o+rw /dev/i2c-0
-chmod o+rw /dev/i2c-1
+sudo chmod o+rw /dev/i2c-0
+sudo chmod o+rw /dev/i2c-1
 source ~/.bashrc
 source ~/catkin_ws/devel/setup.bash
-export ROS_IP=192.168.23.105
-export ROS_MASTER_URI=http://lab_computer:11311
-#export ROS_MASTER_URI=http://localhost:11311
-export ROS_HOSTNAME=mantaray
-#export ROS_HOSTNAME=localhost
+#export ROS_IP=192.168.23.105
+#export ROS_MASTER_URI=http://lab_computer:11311
+export ROS_MASTER_URI=http://localhost:11311
+#export ROS_HOSTNAME=mantaray
+export ROS_HOSTNAME=localhost
 export PYTHONPATH=/home/mantaray/catkin_ws/devel/lib/python2.7/dist-packages:$PYTHONPATH
